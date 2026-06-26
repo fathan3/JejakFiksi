69cbe34198e59147f7378a04e64818953a71b034
fathan3 <fathanra311@gmail.com>
2026-06-26 16:44:07 +0700

    feat: add support for selectable retrieval methods and implement index page UI

--- app.py
+++ app.py
@@ -73,39 +73,42 @@ docs_tokens, doc_lengths, avgdl, idf_bm25 = hitung_bm25_params(dataset)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     query = ""
+    method = "cosine"
     hasil_cosine = []
     hasil_bm25 = []
     if request.method == 'POST':
         query = request.form.get('keyword')
+        method = request.form.get('method', 'cosine')
         query_tokens = preprocess(query)
         
-        # Calculate Cosine Similarity
-        vektor_query = [query_tokens.count(kata) * idf_map.get(kata, 0) for kata in vocab]
-        for i in range(len(vektor_dokumen)):
-            skor = cosine_similarity(vektor_query, vektor_dokumen[i])
-            if skor > 0:
-                hasil_cosine.append({
-                    "judul": dataset[i]['judul'],
-                    "sinopsis": dataset[i]['sinopsis'],
-                    "skor": round(skor, 4),
-                    "url": dataset[i]['url']
-                })
-                
-        # Calculate BM25
-        for i in range(len(dataset)):
-            skor = bm25_score(query_tokens, docs_tokens[i], doc_lengths[i], avgdl, idf_bm25)
-            if skor > 0:
-                hasil_bm25.append({
-                    "judul": dataset[i]['judul'],
-                    "sinopsis": dataset[i]['sinopsis'],
-                    "skor": round(skor, 4),
-                    "url": dataset[i]['url']
-                })
-                    
-        hasil_cosine = sorted(hasil_cosine, key=lambda x: x['skor'], reverse=True)[:10]
-        hasil_bm25 = sorted(hasil_bm25, key=lambda x: x['skor'], reverse=True)[:10]
+        if method == 'cosine':
+            # Calculate Cosine Similarity
+            vektor_query = [query_tokens.count(kata) * idf_map.get(kata, 0) for kata in vocab]
+            for i in range(len(vektor_dokumen)):
+                skor = cosine_similarity(vektor_query, vektor_dokumen[i])
+                if skor > 0:
+                    hasil_cosine.append({
+                        "judul": dataset[i]['judul'],
+                        "sinopsis": dataset[i]['sinopsis'],
+                        "skor": round(skor, 4),
+                        "url": dataset[i]['url']
+                    })
+            hasil_cosine = sorted(hasil_cosine, key=lambda x: x['skor'], reverse=True)[:10]
+            
+        elif method == 'bm25':
+            # Calculate BM25
+            for i in range(len(dataset)):
+                skor = bm25_score(query_tokens, docs_tokens[i], doc_lengths[i], avgdl, idf_bm25)
+                if skor > 0:
+                    hasil_bm25.append({
+                        "judul": dataset[i]['judul'],
+                        "sinopsis": dataset[i]['sinopsis'],
+                        "skor": round(skor, 4),
+                        "url": dataset[i]['url']
+                    })
+            hasil_bm25 = sorted(hasil_bm25, key=lambda x: x['skor'], reverse=True)[:10]
 
-    return render_template('index.html', query=query, hasil_cosine=hasil_cosine, hasil_bm25=hasil_bm25)
+    return render_template('index.html', query=query, method=method, hasil_cosine=hasil_cosine, hasil_bm25=hasil_bm25)
 
 if __name__ == '__main__':
     app.run(debug=True)
